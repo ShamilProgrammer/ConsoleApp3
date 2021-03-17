ee1429cc2305654699c1a9fdc9dcf9ee8a6f09e4
ShamilProgrammer <mirhome2016@gmail.com>
2021-03-17 21:35:00 +0300

    #3 New class "Person" who contains 4 field. Count is count of elements Second is second elements Construction without parameters is needed for initial class

---
+++ ConsoleApp3/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Person
+    {
+        public int Count { get; set; } = 0;
+
+        public int Second { get; set; } = 0;
+
+        public Person()
+        {
+            Count = 1;
+            Second = 2;
+        }
+
+        public void Script()
+        {
+            Console.WriteLine("What are you doing");
+        }
+    }
+}
